aba8d23d7513fd2230dcd6114cbccf978b3bca5e
Marcelo Tyszler <marcelo@blonkconsultants.nl>
2022-12-02 09:12:05 +0100

    Day 1 and initial set-up

---
+++ challenges/day_1.py
@@ -0,0 +1,13 @@
+from main_functions import *
+
+# challenge 1
+elves = parse_input('../input_files/input_day_1.txt')
+max_elf, max_elf_calories = find_max_calories_elf(elves)
+print("elf carrying most calories")
+print(max_elf)
+print(max_elf_calories)
+
+# challenge 2
+df_elves = ranked_calories_elf(elves)
+print("calories of top 3 elves")
+print(sum(df_elves.iloc[0:3, 0]))
---
+++ src/main_functions.py
@@ -0,0 +1,83 @@
+import pandas as pd
+
+
+def parse_input(filename: str) -> dict:
+    """
+     convert input file into a dictionary of dictionaries on elves food
+
+    :param filename: txt file if input of elves food
+    :return:
+        output format:
+        elves = {
+            food = {
+                'food_1': xxx,
+                ...
+
+
+            }
+            total_calories: yyy
+    """
+
+    input_file = open(filename, 'r')
+    lines = input_file.readlines()
+
+    # initialize
+    elves = {}
+    count_elves = 1
+    elves['elf_' + str(count_elves)] = {}
+    elves['elf_' + str(count_elves)]['food'] = {}
+    count_food = 1
+    cum_tot = 0
+
+    for line in lines:
+
+        if line.strip():
+            # 'The line is NOT empty
+            elves['elf_' + str(count_elves)]['food']['food_' + str(count_food)] = line.strip()
+            cum_tot += int(line.strip())
+            count_food += 1
+        else:
+            # The line is empty'
+            elves['elf_' + str(count_elves)]['total_calories'] = cum_tot
+
+            # reset
+            count_elves += 1
+            elves['elf_' + str(count_elves)] = {}
+            elves['elf_' + str(count_elves)]['food'] = {}
+            count_food = 1
+            cum_tot = 0
+
+        if line.strip():
+            # The last line is no empty
+            elves['elf_' + str(count_elves)]['total_calories'] = cum_tot
+
+    return elves
+
+
+def find_max_calories_elf(elves: dict) -> [str, int]:
+    """
+
+    :param elves: see parse_input
+    :return:
+        max_calories, max(total_calories.values())
+    """
+    total_calories = {elf: values['total_calories'] for elf, values in elves.items()}
+    max_calories = [key for key, value in total_calories.items() if value == max(total_calories.values())]
+
+    return max_calories[0], max(total_calories.values())
+
+
+def ranked_calories_elf(elves: dict) -> pd.DataFrame:
+    """
+
+    :param elves: see parse_input
+    :return:
+        max_calories, max(total_calories.values())
+    """
+
+    total_calories = {elf: values['total_calories'] for elf, values in elves.items()}
+    df_elves = pd.DataFrame.from_dict(total_calories, orient='index', columns=['calories']).sort_values(by='calories',
+                                                                                                        ascending=False)
+
+    return df_elves
+
