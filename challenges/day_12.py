69eb9d8b894228b6cb82fe384d632ec9fa8184ee
Marcelo Tyszler <marcelo@blonkconsultants.nl>
2022-12-14 15:46:27 +0100

    drop comment marks

--- challenges/day_12.py
+++ challenges/day_12.py
@@ -1,14 +1,14 @@
 from functions_day_12 import *
 
-# # challenge 1:
-# grid_height = parse_height_map('../input_files/input_day_12.txt')
-# best_distance, best_path = find_top(grid_height)
-#
-# route_map = make_route(grid_height, best_path)
-# np.savetxt('route_map.txt', route_map, fmt='%3i')
-#
-# print("Best distance")
-# print(best_distance)
+# challenge 1:
+grid_height = parse_height_map('../input_files/input_day_12.txt')
+best_distance, best_path = find_top(grid_height)
+
+route_map = make_route(grid_height, best_path)
+np.savetxt('route_map.txt', route_map, fmt='%3i')
+
+print("Best distance")
+print(best_distance)
 
 # challenge 2:
 grid_height = parse_height_map('../input_files/input_day_12.txt')
